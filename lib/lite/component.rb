b35cb2e09454a73104ebf00efe7cde8d7260e5db
Juan Gomez <j.gomez@drexed.com>
2019-12-10 21:59:10 -0500

    Init commit

---
+++ lib/lite/component.rb
@@ -0,0 +1,8 @@
+require "lite/component/version"
+
+module Lite
+  module Component
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/lite/component/version.rb
@@ -0,0 +1,5 @@
+module Lite
+  module Component
+    VERSION = "0.1.0"
+  end
+end
